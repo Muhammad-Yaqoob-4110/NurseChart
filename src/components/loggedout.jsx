1a7a138e93c559686d6b4d18caad3ca34018f085
Muhammad-Yaqoob-4110 <my6580861@gmail.com>
2024-08-15 10:19:35 +0500

    Make Logged Out page Responsive

--- src/components/loggedout.jsx
+++ src/components/loggedout.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Loggedout extends Component {
     render() {
         return (
             <div className='w-full h-full mt-8'>
                 <div className='flex justify-center items-center'>
-                    <div className='h-[300px] w-[700px] bg-white rounded-[7px] flex flex-col justify-center items-center gap-2'>
+                    <div className='lg:w-[632px] w-11/12 p-6 bg-white rounded-[7px] flex flex-col justify-center items-center gap-2'>
                         <h2 className='font-bold text-[#0F7573FF] text-2xl p-4'>Logged Out</h2>
-                        <h5 className='text-[#615F63FF] p-1'>You have successfully logged out of NurseChart</h5>
+                        <h5 className='text-[#615F63FF] p-1 text-center'>You have successfully logged out of NurseChart</h5>
                         <div className='flex flex-col text-[#A3309BFF]'>
-                        <a className='hover:underline' href="">Click here to log back in</a>
+                        <Link className='hover:underline' to="/login">Click here to log back in</Link>
                         <a className='hover:underline' href="">Return to Home Page</a>
                         </div>
                     </div>
