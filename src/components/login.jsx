7935a642fc0220df84bb9e76405e7a43d9701ba9
Muhammad-Yaqoob-4110 <my6580861@gmail.com>
2024-08-15 09:39:26 +0500

    Make login page Responsive

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -6,7 +6,7 @@ class Login extends Component {
         return (
             <div className='w-full h-full'>
                 <div className='flex justify-center items-center mt-8'>
-                    <div className='h-[400px] w-[632px] p-6 bg-white rounded-[7px]'>
+                    <div className='lg:w-[632px] w-11/12 p-6 bg-white rounded-[7px]'>
                         <h4 className="p-4 text-center font-bold text-lg text-[#00817FFF]"> NurseChart</h4>
                         <div className='flex flex-col gap-2'>
                             <label className="relative block">
@@ -14,29 +14,29 @@ class Login extends Component {
                                 <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                                     <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
                                 </span>
-                                <input className="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Username" type="text" name="search" />
+                                <input className="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-[#00817FFF] focus:ring-[#00817FFF] focus:ring-1 sm:text-sm" placeholder="Username" type="text" name="search" />
                             </label>
                             <label className="relative block">
                                 <span className="sr-only">Username</span>
                                 <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                                     <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
                                 </span>
-                                <input className=" placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Passowrd" type="text" name="search" />
+                                <input className=" placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-[#00817FFF] focus:ring-[#00817FFF] focus:ring-1 sm:text-sm" placeholder="Passowrd" type="text" name="search" />
                             </label>
                         </div>
 
                         <div className='flex mt-2 justify-center'>
-                            <button class="bg-[#00817FFF] cursor-pointer py-2 px-5 text-white rounded">
+                            <Link class="bg-[#00817FFF] cursor-pointer py-2 px-5 text-white rounded" to="/home">
                                 Log in
-                            </button>
+                            </Link>
                         </div>
 
-                        <div className='flex flex-col mt-4 gap-3'>
+                        <div className='flex text-sm flex-col mt-4 gap-3'>
                         <div className='flex gap-1 justify-center'>
                             <a className='text-blue-600 hover:underline' href="">Forget Password</a>
-                            <p className='text-blue-600'>|</p>
+                            <p className='bg-[#00817FFF] w-[1px]'></p>
                             <Link className='text-blue-600 hover:underline' to="/register">Register New Account</Link>
-                            <p className='text-blue-600'>|</p>
+                            <p className='bg-[#00817FFF] w-[1px]'></p>
                             <a className='text-blue-600 hover:underline' href="">Support</a>
                         </div>
                         <p className='text-center'>Effortless Documentation for Compassionate Care</p>
