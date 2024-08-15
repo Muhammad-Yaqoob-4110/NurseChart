37c0cb5eba518def910a9fbb9b220a4dc18025c5
Muhammad-Yaqoob-4110 <my6580861@gmail.com>
2024-08-15 09:38:06 +0500

    Update navbar to make it responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <div className="w-full lg:p-4 text-center text-xl font-bold navbar-text-color text-[#504F50FF] bg-white">
+            <div className="w-full p-4 text-center text-xl font-bold navbar-text-color text-[#504F50FF] bg-white">
                 {this.props.text}
             </div>
         );
