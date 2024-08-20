bec790ea1ab4a15f55a92f2d72e5e86359477dd6
Muhammad-Yaqoob-4110 <my6580861@gmail.com>
2024-08-20 11:51:50 +0500

    Add loading at Vit Signs Template downloading

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -10,7 +10,8 @@ class Home extends Component {
     this.state = {
       isRecording: false,
       transcription: '',
-      selectedTemplate: ''
+      selectedTemplate: '',
+      isLoading: false,
     };
     this.recognition = null;
     this.textAreaRef = React.createRef();
@@ -98,7 +99,7 @@ class Home extends Component {
     // Generate the response
     // Check if the selected option is "Vital Signs"
     if (selectedTemplate === "Vital Signs") {
-
+      this.setState({ isLoading: true });
       // Define the text from which you want to extract details
       const text = `
       Patient's vital signs are as follows: Temperature is 98.6°F, Pulse is 72 bpm, Respiration rate is 16 breaths per minute, Blood pressure is 120/80 mmHg, Oxygen saturation is 98%, Pain level is 3 out of 10, and Blood sugar level is 90 mg/dL.
@@ -117,7 +118,8 @@ class Home extends Component {
 
       Text: ${text}
       `;
-      async function run() {
+      const run = async () => {
+        try {
         // const prompt = "Write a story about an AI and magic"
         const result = await model.generateContent(prompt);
         const response = await result.response;
@@ -145,12 +147,13 @@ class Home extends Component {
         doc.text(splitText, margin, 35, { maxWidth: 180, align: 'left' });
 
         doc.save("VitalSignsReport.pdf");
-
-        // const doc = new jsPDF();
-        // doc.text(text, 10, 10);
-        // doc.save("VitalSignsReport.pdf");
       }
+      finally {
+        this.setState({ isLoading: false });
+      }
+    } 
       run();
+
     }
     if (selectedTemplate === "Head-to-Toe Assessment") {
       const headToToeText = `
@@ -216,80 +219,90 @@ class Home extends Component {
   }
 
   render() {
-    const { isRecording, transcription } = this.state;
+    const { isRecording, transcription, isLoading } = this.state;
     return (
-      <div className='w-full h-full'>
-        <div className='flex justify-center items-center mt-8'>
-          <div className='lg:w-[632px] w-11/12 p-6 bg-white rounded-[7px]'>
-            <div className=" flex bg-[#00817FFF] p-4">
-              <div className='w-4/5 '>
-                <h4 className="text-center font-bold text-lg text-white">NurseChart</h4>
+      <div className='relative'>
+        {isLoading && (
+                    <div className="absolute bg-white bg-opacity-60 z-10 h-full w-full flex items-center justify-center">
+                        <div className="flex items-center">
+                            <span className="text-3xl mr-4"></span>
+                            <svg className="animate-spin h-8 w-8 text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                <path className="opacity-75" fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                )}
+        <div className='w-full h-full'>
+          <div className='flex justify-center items-center mt-8'>
+            <div className='lg:w-[632px] w-11/12 p-6 bg-white rounded-[7px]'>
+              <div className=" flex bg-[#00817FFF] p-4">
+                <div className='w-4/5 '>
+                  <h4 className="text-center font-bold text-lg text-white">NurseChart</h4>
+                </div>
+                <div className='w-1/5  flex justify-end'>
+                  <Link class="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white" to="/loggedout">
+                    Logout
+                  </Link>
+                </div>
               </div>
-              <div className='w-1/5  flex justify-end'>
-                <Link class="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white" to="/loggedout">
-                  Logout
-                </Link>
-              </div>
-            </div>
-
-
-            <div className='flex lg:flex-row flex-col gap-1 grid-cols-2 mt-1'>
-              {/* Left Section */}
-              <div className='lg:w-1/4 '>
-                <ul className='flex flex-col gap-0'>
-                  <p>Patient List</p>
-
-                  <a className='text-blue-600 hover:cursor-pointer hover:underline'>Ali Ahmed</a>
-                  <a className='text-blue-600 hover:cursor-pointer hover:underline'>Ali Ahmed</a>
-                  <a className='text-blue-600 hover:cursor-pointer hover:underline'>Ali Ahmed</a>
-                  <a className='text-blue-600 hover:cursor-pointer hover:underline'>See More...</a>
-                </ul>
-                <button className="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white w-full mt-2">
-                  Add New Patient
-                </button>
-              </div>
-              {/* Right Section */}
-              <div className='flex flex-col gap-2 lg:w-3/4 '>
-                <p>Quick Templates</p>
-                <div className='flex gap-2 lg:text-base text-sm'>
-                  <select className='border focus:outline-none' value={this.state.selectedTemplate}
-                    onChange={this.handleSelectChange}>
-                    <option value="">Select a Template</option>
-                    <option value="Vital Signs">Vital Signs</option>
-                    <option value="Head-to-Toe Assessment">Head-to-Toe Assessment</option>
-                    <option value="Medication Administration">Medication Administration</option>
-                    <option value="Specialized Care">Specialized Care</option>
-                    <option value="Patient Education">Patient Education</option>
-                    <option value="Plan for Next Visit">Plain for Next Visit</option>
-                  </select>
-                  <button onClick={this.handleGenerateReport} className="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white">
-                    Generate Report
+              <div className='flex lg:flex-row flex-col gap-1 grid-cols-2 mt-1'>
+                {/* Left Section */}
+                <div className='lg:w-1/4 '>
+                  <ul className='flex flex-col gap-0'>
+                    <p>Patient List</p>
+                    <a className='text-blue-600 hover:cursor-pointer hover:underline'>Ali Ahmed</a>
+                    <a className='text-blue-600 hover:cursor-pointer hover:underline'>Ali Ahmed</a>
+                    <a className='text-blue-600 hover:cursor-pointer hover:underline'>Ali Ahmed</a>
+                    <a className='text-blue-600 hover:cursor-pointer hover:underline'>See More...</a>
+                  </ul>
+                  <button className="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white w-full mt-2">
+                    Add New Patient
                   </button>
                 </div>
-                <p>Voice Recording</p>
-
-                <div className='flex gap-2 '>
-                  <button class="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white" onClick={this.handleRecording}>
-                    {isRecording ? 'Recording...' : 'Start Recording'}
-                  </button>
-                  <button class="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white" onClick={this.handleStopRecording}>
-                    Stop Recording
-                  </button>
+                {/* Right Section */}
+                <div className='flex flex-col gap-2 lg:w-3/4 '>
+                  <p>Quick Templates</p>
+                  <div className='flex gap-2 lg:text-base text-sm'>
+                    <select className='border focus:outline-none' value={this.state.selectedTemplate}
+                      onChange={this.handleSelectChange}>
+                      <option value="">Select a Template</option>
+                      <option value="Vital Signs">Vital Signs</option>
+                      <option value="Head-to-Toe Assessment">Head-to-Toe Assessment</option>
+                      <option value="Medication Administration">Medication Administration</option>
+                      <option value="Specialized Care">Specialized Care</option>
+                      <option value="Patient Education">Patient Education</option>
+                      <option value="Plan for Next Visit">Plain for Next Visit</option>
+                    </select>
+                    <button onClick={this.handleGenerateReport} className="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white">
+                      Generate Report
+                    </button>
+                  </div>
+                  <p>Voice Recording</p>
+                  <div className='flex gap-2 '>
+                    <button class="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white" onClick={this.handleRecording}>
+                      {isRecording ? 'Recording...' : 'Start Recording'}
+                    </button>
+                    <button class="bg-[#910086FF] rounded-sm cursor-pointer p-1 text-white" onClick={this.handleStopRecording}>
+                      Stop Recording
+                    </button>
+                  </div>
+                  <textarea ref={this.textAreaRef} className='border resize-none p-1 border-[#DEDEDEFF] focus:outline-[#DEDEDEFF] focus:border-transparent' placeholder='Transcription will appear here.' id="transcription" name="transcription" value={transcription} readOnly rows="6" cols="50">
+                  </textarea>
                 </div>
-                <textarea ref={this.textAreaRef} className='border resize-none p-1 border-[#DEDEDEFF] focus:outline-[#DEDEDEFF] focus:border-transparent' placeholder='Transcription will appear here.' id="transcription" name="transcription" value={transcription} readOnly rows="6" cols="50">
-                </textarea>
               </div>
             </div>
-
           </div>
-        </div>
-        <div className='flex gap-2 justify-center mt-3'>
-          <button class="bg-[#00817FFF] cursor-pointer py-2 px-4 text-white rounded">
-            Previous
-          </button>
-          <button className="bg-[#00817FFF] cursor-pointer py-2 px-4 text-white rounded">
-            Next
-          </button>
+          <div className='flex gap-2 justify-center mt-3'>
+            <button class="bg-[#00817FFF] cursor-pointer py-2 px-4 text-white rounded">
+              Previous
+            </button>
+            <button className="bg-[#00817FFF] cursor-pointer py-2 px-4 text-white rounded">
+              Next
+            </button>
+          </div>
         </div>
       </div>
     );
