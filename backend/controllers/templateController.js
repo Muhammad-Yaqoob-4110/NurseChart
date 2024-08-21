a81fa888d3be8d3c925300f20e50f0cc258debff
Muhammad-Yaqoob-4110 <my6580861@gmail.com>
2024-08-21 17:00:26 +0500

    Add get All templates for a specific user

--- backend/controllers/templateController.js
+++ backend/controllers/templateController.js
@@ -16,7 +16,30 @@ async function createTemplate(req, res) {
     }
   }
 
+  // Get all Templates
+async function getAllTemplates(req, res) {
+  try {
+    const { userid } = req.body;
+
+    // Find templates by userid
+    const templates = await Template.find({ userid: userid });
+    
+    // console.log(templates)
+    // Extract relevant data from the templates
+    const templatesDict = templates.map(tem => ({
+      templateid: tem.templateid,
+      template: tem.template,
+    }));
+    
+    // Return the list of usernames as JSON
+    res.json(templatesDict);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
   module.exports = {
     createTemplate,
+    getAllTemplates,
   };
   
--- backend/routes/templateRoutes.js
+++ backend/routes/templateRoutes.js
@@ -5,4 +5,7 @@ const templateController = require("../controllers/templateController");
 // create a template
 router.post("/templates", templateController.createTemplate);
 
+// create a template
+router.get('/templates', templateController.getAllTemplates);
+
 module.exports = router;
