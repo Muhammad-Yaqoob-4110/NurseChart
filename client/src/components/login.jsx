1643b856836e363b3100621f1b2d8512779c48dd
Muhammad-Yaqoob-4110 <my6580861@gmail.com>
2024-08-26 18:44:42 +0500

    Add Show and Hide Password

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -187,7 +187,7 @@ class Login extends Component {
                                         <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                                             <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
                                         </span>
-                                        <input className="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-[#00817FFF] focus:ring-[#00817FFF] focus:ring-1 sm:text-sm" placeholder="Username" type="text" name="username" value={this.state.user.username} onChange={this.handleChange} />
+                                        <input className="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-[#00817FFF] focus:ring-[#00817FFF] focus:ring-1 sm:text-sm" placeholder="Username" type="text" name="username" value={this.state.user.username} onChange={this.handleChange} />
                                     </label>
                                     {this.state.errors.username && (
                                         <div className="text-sm text-red-600">{this.state.errors.username}</div>
@@ -197,7 +197,7 @@ class Login extends Component {
                                         <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                                             <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
                                         </span>
-                                        <input className=" placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-[#00817FFF] focus:ring-[#00817FFF] focus:ring-1 sm:text-sm" placeholder="Passowrd" type="text" name="password" value={this.state.user.password} onChange={this.handleChange} />
+                                        <input className=" placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-[#00817FFF] focus:ring-[#00817FFF] focus:ring-1 sm:text-sm" placeholder="Passowrd" type="password" name="password" value={this.state.user.password} onChange={this.handleChange} />
                                     </label>
                                     {this.state.errors.password && (
                                         <div className="text-sm text-red-600">{this.state.errors.password}</div>
